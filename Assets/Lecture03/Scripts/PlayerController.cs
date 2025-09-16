48607086a0afd786bf9848f67218b19356167db5
elflee77 <elflee77@gmail.com>
2025-09-16 10:44:55 +0900

    .

---
+++ Assets/Lecture03/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Debug.Log("Space Key Pressed!");
+            rb.linearVelocity = new Vector2(0.0f, 10.0f);
+        }
+    }
+}
